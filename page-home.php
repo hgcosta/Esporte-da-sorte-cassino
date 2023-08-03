b34e2190702cd654d346ba9e62648f0f67e61f96
hgcosta <hugocosta.devs@gmail.com>
2023-08-02 21:04:01 -0300

    feature: sessao hero, todos jogos e article principal

--- page-home.php
+++ page-home.php
@@ -1,6 +1,147 @@
 <?php // Template Name: Home ?> 
 <?php get_header()?>
+<section class="hero">
+    <header>
+        <div class="container content-header">
+            <div class="logo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/logo-esporte-da-sorte-header.png" alt=""></div>
+            <nav>
+                <ul class="list-unstyled">
+                    <li><a href="">Esportes </br>da Sorte</a></li>
+                    <li><a href="">Bônus</a></li>
+                    <li><a href="">App</a></li>
+                    <li><a href="">Freebet</a></li>
+                    <li><a href="">Ao vivo</a></li>
+                    <li><a href="">Futebol</a></li>
+                    <li><a href="">UFC</a></li>
+                    <li><a href="">Cadastro</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="container content-hero">
+        <div class="col-info">
+            <h1>Cassino e apostas esportivas</h1>
+            <p>Apostas ao vivo, saque rápido, live Cassino e muito mais!</p>
+            <div class="btns-hero">
+                <a href="">Cadastre-se</a>
+                <a href="">Bônus</a>
+            </div>
+            <div class="info-secundaria">
+                <p>Ganhe até R$ 300 no seu primeiro depósito!</p>
+            </div>
+        </div>
+        <div class="col-img">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/influencers-hero.png" alt="">
+        </div>
+    </div>
+</section>
+<section class="jogos">
+   <div class="container content-jogos">
+    <h2>Todos os Jogos</h2>
+        <ul class="list-unstyled">
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/fortune-tiger.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/fortune-x.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/fortune-mouse.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rab.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/space.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/avi.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/double.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sweet.png" alt=""></a></li>
+            <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reel-love.png" alt=""></a></li>
+        </ul>
+        <div class="vertodos">
+            <a href="">Veja todos os jogos</a>
+        </div>
+        <div class="jogue-agora">
+            <a class="absolute-button" href="">Jogue <strong>Agora</strong></a>
+        </div>
+   </div>
+</section>
+<section class="section-texto-geral">
+    <div class="container content-info">
+        <article>
+            <h3>O que é a Esportes da Sorte Cassino?</h3>
+            <p>A Esporte da Sorte é tanto um cassino online como também casa de apostas esportivas que oferece uma variedade incrível de modalidades, com destaque para  o Esporte da Sorte UFC. Além das apostas em esportes ao vivo, a Esporte da Sorte apostas também disponibiliza diversos tipos de jogos de cassino,  desde os slots online como o Fortune Tiger e o Fortune Mouse, até jogos de cassino ao vivo e crash para apostar como o Esporte da Sorte Spaceman, para diversificar suas opções e aumentar suas chances de ganho. Com a Esporte da Sorte, você tem tudo o que precisa para transformar seu amor pelo futebol em lucro!</p>
+            <div class="jogue-agora">
+                <a href="">Jogue <strong>Agora</strong></a>
+            </div>
+        </article>
+        <article>
+            <h3>Detalhes da Esporte da Sorte</h3>
+            <p>A Esportes da Sorte Casino é uma operadora online de apostas esportivas confiável e completa, que oferece diversas categorias esportivas, melhores odds do mercado, jogos múltiplos com cotação superior e a possibilidade de prêmios de até 1 milhão de reais. Além disso, o site conta com uma equipe de suporte altamente treinada para auxiliar em todas as dúvidas e melhorar a experiência do usuário durante a jogatina de qualquer jogo de cassino ou em apostas esportivas, como futebol. Confira abaixo as especificações técnicas da Esporte da Sorte:</p>
+            <ul class="lista-padrao">
+                <li>Ano de criação: 2022</li>
+                <li>Serviços de atendimento: Chat, FAQ e e-mail</li>
+                <li>Depósito mínimo: R$ 1,00</li>
+                <li>Método de depósito: Pix</li>
+            </ul>
+            <div class="cadastre-se">
+                <a class="absolute-button" href="">Cadastre-se</a>
+            </div>
+        </article>
+    </div>
+</section>
+<section class="section-texto-secundario">
+    <div class="container">
 
+        <article class="texto-imagem">
+            <div class="col-texto">
+                <h3>Esporte da Sorte cadastro</h3>
+                <p>A Esporte da Sorte acredita que o processo de registro deve ser simples e rápido, permitindo que você comece a jogar e a ganhar o quanto antes. Com apenas alguns cliques, você pode criar sua conta e começar a explorar o universo dos jogos e apostas esportivas. Além disso, nos orgulhamos de nossa transparência e segurança, garantindo que suas informações estejam sempre protegidas. Siga nosso guia passo a passo abaixo e embarque conosco nessa aventura!</p>
+                <ul class="lista-numerica">
+                    <li>Acesse o site da Esporte da Sorte clique em “Criar Conta”;</li>
+                    <li>Preencha o formulário com nome, CPF, e-mail, telefone, nome de usuário e senha;</li>
+                    <li>Verifique se as informações estão corretas;</li>
+                    <li>Marque a opção “Concordo” e clique em “Criar Conta”;</li>
+                    <li>Confirme o seu e-mail</li>
+                </ul>
+            </div>
+            <div class="col-img">
+                <img src="" alt="">
+            </div>
+            <a href="">Cadastre-se</a>
+        </article>
 
+        <article class="texto">
+            <h3>A Esporte da Sorte é confiável?</h3>
+            <p>A confiabilidade de um site de apostas online é medida por vários critérios, entre os quais destacam-se as licenças e certificações de operação e o comprometimento da empresa com a transparência e a segurança dos usuários. A Esporte da Sorte é operada pela HSF Gaming N.V., empresa registrada em Curaçao sob o número 160747.</p>
+            <p>Possui uma licença de jogo concedida pelo General Governor of Curaçao sob o número 8048/JAZ2021-170. Isso significa que a Esporte da Sorte está em total conformidade com os padrões internacionais de segurança e justiça para jogos online.</p>
+            <p>Além disso, a Esporte da Sorte demonstra seu comprometimento com o futebol e os esportes em geral através do patrocínio de vários times brasileiros, como Athletico-PR, Grêmio, Goiás, Bahia, ABC, Guarani, Londrina, Grêmio Novohorizontino e Vila Nova. A empresa também apoiou competições esportivas como o Campeonato Paulista 2023 e a Copinha de 2023. Essas iniciativas refletem o compromisso da Esporte da Sorte com o esporte e reforçam a confiabilidade da plataforma.</p>
+        </article>
+
+        <article class="texto-imagem">
+            <div class="col-texto">
+                <h3>Esporte da Sorte no Reclame Aqui</h3>
+                <p>O site Reclame Aqui é uma referência no Brasil para avaliar a qualidade do atendimento ao cliente e a resolução de problemas de empresas de todos os setores. A Esporte da Sorte possui 5400 reclamações registradas no site, e todas foram respondidas. Isso é um indicativo do compromisso da empresa em resolver as questões de seus usuários e fornecer um atendimento ao cliente eficiente e responsivo.</p>
+                <p>As reclamações mais comuns na plataforma são de usuários que não conseguem retirar o valor dos bônus e promoções. É importante lembrar que tais bônus possuem requisitos de aposta (rollover) que devem ser cumpridos antes da retirada. No caso da Esporte da Sorte, o bônus precisa ser apostado 40 vezes em um período de 15 dias. Essa informação é disponibilizada nas condições de uso dos bônus, reforçando a importância de ler e entender esses termos antes de optar pelas promoções. A Esporte da Sorte se empenha em oferecer uma comunicação clara e transparente a seus usuários, reafirmando sua confiabilidade como plataforma de apostas online.</p>    
+            </div>
+            <div class="col-img">
+                <img src="" alt="">
+            </div>
+            <a href="">Cadastre-se</a>
+        </article>
+
+        <article class="texto">
+            <h3>Esporte da Sorte vantagens</h3>
+            <p>Se você está pensando em se inscrever na Esporte da Sorte, é importante entender as vantagens e desvantagens da plataforma de apostas antes de começar a jogar. Embora a Esporte da Sorte seja uma das maiores e mais populares plataformas de apostas online, há prós e contras que você deve considerar antes de fazer sua primeira aposta.</p>
+            <p>A Esporte da Sorte se destaca no mercado de apostas esportivas e jogos de cassino online não só pela variedade e qualidade dos jogos oferecidos, mas também pelas inúmeras vantagens que oferecemos aos nossos usuários. A Esporte da Sorte está em constante crescimento e evolução, sempre buscando melhorar e inovar para proporcionar a melhor experiência possível. E, apesar de algumas desvantagens, como a ausência de um Esporte da Sorte app e apk para celular, a Esporte da Sorte Casino trabalha incansavelmente para corrigir esses pontos e oferecer um serviço cada vez melhor.</p>
+            <p>Confira abaixo as vantagens e da Esporte da Sorte:</p>
+            <ul >
+                <li>É uma dos cassinos online que mais crescem no Brasil;</li>
+                <li>Oferece uma grande variedade de mercados de apostas esportivas;</li>
+                <li>Bônus Esporte da Sorte generoso;</li>
+                <li>Disponibiliza a opção de apostas ao vivo;</li>
+                <li>Você pode apostar também no cassino da Esporte da Sorte.</li>
+                <li>É dos mesmos criadores da Ona Bet Casino</li>
+            </ul>
+        </article>
+
+        <article class="texto">
+            <h3>Atendimento ao cliente Esporte da Sorte</h3>
+            <p>O Esporte da Sorte oferece diversas opções de atendimento ao cliente para esclarecer dúvidas e resolver questões relacionadas às apostas esportivas. Os usuários podem acessar a seção de FAQ no site da plataforma, que traz respostas para as perguntas mais frequentes. Além disso, a opção de chat ao vivo é uma maneira rápida e eficiente de obter suporte personalizado de um dos atendentes da plataforma. O Esporte da Sorte também oferece a opção de contato via e-mail para questões mais complexas que requerem uma resposta mais detalhada.</p>
+        </article>
+        
+    </div>
+</section>
 
 <?php get_footer()?>
